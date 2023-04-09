cb2c2d57076370cdfd6fb28a98b061533f2819d3
Sarah <safanothman@gmail.com>
2023-04-10 00:15:25 +0300

    اdd

--- app/src/main/java/com/example/myapplication/home.java
+++ app/src/main/java/com/example/myapplication/home.java
@@ -2,13 +2,26 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class home extends AppCompatActivity {
+public class home  extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ImageButton chat = findViewById(R.id.chat);
+        chat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(home.this, chat.class);
+                startActivity(intent);
+            }
+        });
+
     }
-}
+    }
+

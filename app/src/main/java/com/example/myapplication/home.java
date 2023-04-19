8fd13ee2dcce9fb38a5a19343407b0ec7620e12d
Deema <Deemaalsahow@gmail.com>
2023-04-19 06:35:44 +0300

    Driver

--- app/src/main/java/com/example/myapplication/home.java
+++ app/src/main/java/com/example/myapplication/home.java
@@ -5,57 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 
-public class home extends AppCompatActivity {
+public class home  extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
-        Button button= findViewById(R.id.butchat);
-        button.setOnClickListener(new View.OnClickListener() {
+        ImageButton chat = findViewById(R.id.chat);
+        chat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                Intent intent=new Intent(home.this,chat.class);
+                Intent intent = new Intent(home.this, chat.class);
                 startActivity(intent);
-
-            }
-        });
-        Button button1=findViewById(R.id.butcal);
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(home.this,Absent.class);
-                startActivity(intent);
-
             }
         });
 
-        Button button2=findViewById(R.id.butdri);
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(home.this,driverinformation.class);
-                startActivity(intent);
-
-
-            }
-        });
-
-        ImageButton imageButton=findViewById(R.id.profile);
-        imageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(home.this,profileinfo.class);
-                startActivity(intent);
-
-
-
-            }
-        });
     }
-}
+    }
+
